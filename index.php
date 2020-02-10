8c8c17bbf7fba90adce4c9661eaee89ecb465f15
DonMed <xmena97@gmail.com>
2020-02-10 14:33:46 +0100

    index creado

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title>LIGA BALONCESTO</title>
+<link rel="stylesheet" type="text/css" href="estilos.css?1.0" media="all" >
+</head>
+<body>
+<h1>Acceso Liga Baloncesto</h1>
+<?php
+
+?>
+<form action="principal.php" method="post">
+ <p>Usuario: <input type="text" required name="user" /></p>
+ <p>Password: <input type="password" required name="password" /></p>
+ <p><input type="submit" /></p>
+</form>
+</body>
+</html>
