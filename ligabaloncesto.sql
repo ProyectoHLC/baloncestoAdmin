802b26761f413d4949075e276bb161fda3f9efc9
Ismael <ismaelreyescaballero@gmail.com>
2020-02-11 20:26:34 +0100

    Base de datos y entidad-relación

---
+++ ligabaloncesto.sql
@@ -0,0 +1,137 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 11-02-2020 a las 20:22:16
+-- Versión del servidor: 10.4.11-MariaDB
+-- Versión de PHP: 7.4.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `ligabaloncesto`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `equipos`
+--
+
+CREATE TABLE `equipos` (
+  `cod_equipo` int(11) NOT NULL,
+  `cod_liga` int(11) NOT NULL,
+  `nombre` varchar(45) NOT NULL,
+  `ciudad` varchar(45) NOT NULL,
+  `num_social` int(11) NOT NULL,
+  `fecha` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `equipos`
+--
+
+INSERT INTO `equipos` (`cod_equipo`, `cod_liga`, `nombre`, `ciudad`, `num_social`, `fecha`) VALUES
+(1, 1, 'Equipo de prueba 1', 'Madrid', 12345, '2020-02-05'),
+(2, 1, 'Equipo de prueba 2', 'Cádiz', 54321, '2019-09-18');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `liga`
+--
+
+CREATE TABLE `liga` (
+  `cod_liga` int(11) NOT NULL,
+  `nombre` varchar(45) NOT NULL,
+  `year` date NOT NULL,
+  `descripcion` varchar(140) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `liga`
+--
+
+INSERT INTO `liga` (`cod_liga`, `nombre`, `year`, `descripcion`) VALUES
+(1, 'Liga de Baloncesto', '2020-02-11', 'Liga de baloncesto de prueba para el proyecto final de HLC.');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `resultados`
+--
+
+CREATE TABLE `resultados` (
+  `cod_equipo1` int(11) NOT NULL,
+  `cod_equipo2` int(11) NOT NULL,
+  `result_equipo1` int(11) NOT NULL,
+  `result_equipo2` int(11) NOT NULL,
+  `fecha` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `username` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `equipos`
+--
+ALTER TABLE `equipos`
+  ADD PRIMARY KEY (`cod_equipo`),
+  ADD KEY `fk_liga` (`cod_liga`);
+
+--
+-- Indices de la tabla `liga`
+--
+ALTER TABLE `liga`
+  ADD PRIMARY KEY (`cod_liga`);
+
+--
+-- Indices de la tabla `resultados`
+--
+ALTER TABLE `resultados`
+  ADD KEY `fk_equipo_1` (`cod_equipo1`),
+  ADD KEY `fk_equipo_2` (`cod_equipo2`);
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `equipos`
+--
+ALTER TABLE `equipos`
+  ADD CONSTRAINT `fk_liga` FOREIGN KEY (`cod_liga`) REFERENCES `liga` (`cod_liga`);
+
+--
+-- Filtros para la tabla `resultados`
+--
+ALTER TABLE `resultados`
+  ADD CONSTRAINT `fk_equipo_1` FOREIGN KEY (`cod_equipo1`) REFERENCES `equipos` (`cod_equipo`),
+  ADD CONSTRAINT `fk_equipo_2` FOREIGN KEY (`cod_equipo2`) REFERENCES `equipos` (`cod_equipo`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
