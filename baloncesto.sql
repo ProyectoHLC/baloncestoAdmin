4988c57c25d5e8229c84aa0a4011a3a056901a3f
Ismael <ismaelreyescaballero@gmail.com>
2020-03-05 17:25:00 +0100

    Sql con id autoincrementado

--- baloncesto.sql
+++ baloncesto.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 05-03-2020 a las 17:19:44
+-- Tiempo de generación: 05-03-2020 a las 17:24:09
 -- Versión del servidor: 10.4.11-MariaDB
 -- Versión de PHP: 7.4.1
 
@@ -37,15 +37,6 @@ CREATE TABLE `equipos` (
   `fecha` year(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Volcado de datos para la tabla `equipos`
---
-
-INSERT INTO `equipos` (`cod_equipo`, `cod_liga`, `nombre`, `ciudad`, `num_social`, `fecha`) VALUES
-(1, 1, 'Equipo de prueba 1', 'Madrid', 12345, 2020),
-(2, 1, 'Equipo de prueba 2', 'Cádiz', 54321, 2019),
-(3, 1, '111', '111', 111, 0000);
-
 -- --------------------------------------------------------
 
 --
@@ -58,13 +49,6 @@ CREATE TABLE `liga` (
   `year` year(4) NOT NULL,
   `descripcion` varchar(140) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Volcado de datos para la tabla `liga`
---
-
-INSERT INTO `liga` (`cod_liga`, `nombre`, `year`, `descripcion`) VALUES
-(1, 'Liga de baloncesto', 2020, 'Liga de baloncesto de prueba para el proyecto final de HLC.');
 
 -- --------------------------------------------------------
 
@@ -81,14 +65,6 @@ CREATE TABLE `resultados` (
   `fecha` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Volcado de datos para la tabla `resultados`
---
-
-INSERT INTO `resultados` (`id_result`, `cod_equipo1`, `cod_equipo2`, `result_equipo1`, `result_equipo2`, `fecha`) VALUES
-(1, 1, 2, 1, 2, '0000-00-00'),
-(7, 1, 2, 12, 2, '0000-00-00');
-
 -- --------------------------------------------------------
 
 --
@@ -99,13 +75,6 @@ CREATE TABLE `usuarios` (
   `username` varchar(30) NOT NULL,
   `password` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Volcado de datos para la tabla `usuarios`
---
-
-INSERT INTO `usuarios` (`username`, `password`) VALUES
-('Baldomero', '12345678');
 
 --
 -- Índices para tablas volcadas
@@ -140,7 +109,7 @@ ALTER TABLE `resultados`
 -- AUTO_INCREMENT de la tabla `resultados`
 --
 ALTER TABLE `resultados`
-  MODIFY `id_result` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_result` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Restricciones para tablas volcadas
