39a73dec807c6844a2d57493ed68e438ba9aa9a1
Ismael <ismaelreyescaballero@gmail.com>
2020-03-05 17:23:52 +0100

    Sql con id autoincrementado

--- baloncesto.sql
+++ baloncesto.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 17-02-2020 a las 14:06:40
+-- Tiempo de generación: 05-03-2020 a las 17:19:44
 -- Versión del servidor: 10.4.11-MariaDB
 -- Versión de PHP: 7.4.1
 
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `ligabaloncesto`
+-- Base de datos: `baloncesto`
 --
 
 -- --------------------------------------------------------
@@ -43,7 +43,8 @@ CREATE TABLE `equipos` (
 
 INSERT INTO `equipos` (`cod_equipo`, `cod_liga`, `nombre`, `ciudad`, `num_social`, `fecha`) VALUES
 (1, 1, 'Equipo de prueba 1', 'Madrid', 12345, 2020),
-(2, 1, 'Equipo de prueba 2', 'Cádiz', 54321, 2019);
+(2, 1, 'Equipo de prueba 2', 'Cádiz', 54321, 2019),
+(3, 1, '111', '111', 111, 0000);
 
 -- --------------------------------------------------------
 
@@ -63,7 +64,7 @@ CREATE TABLE `liga` (
 --
 
 INSERT INTO `liga` (`cod_liga`, `nombre`, `year`, `descripcion`) VALUES
-(1, 'Liga de Baloncesto', 2020, 'Liga de baloncesto de prueba para el proyecto final de HLC.');
+(1, 'Liga de baloncesto', 2020, 'Liga de baloncesto de prueba para el proyecto final de HLC.');
 
 -- --------------------------------------------------------
 
@@ -72,12 +73,21 @@ INSERT INTO `liga` (`cod_liga`, `nombre`, `year`, `descripcion`) VALUES
 --
 
 CREATE TABLE `resultados` (
+  `id_result` int(11) NOT NULL,
   `cod_equipo1` int(11) NOT NULL,
   `cod_equipo2` int(11) NOT NULL,
   `result_equipo1` int(11) NOT NULL,
   `result_equipo2` int(11) NOT NULL,
   `fecha` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `resultados`
+--
+
+INSERT INTO `resultados` (`id_result`, `cod_equipo1`, `cod_equipo2`, `result_equipo1`, `result_equipo2`, `fecha`) VALUES
+(1, 1, 2, 1, 2, '0000-00-00'),
+(7, 1, 2, 12, 2, '0000-00-00');
 
 -- --------------------------------------------------------
 
@@ -89,6 +99,13 @@ CREATE TABLE `usuarios` (
   `username` varchar(30) NOT NULL,
   `password` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`username`, `password`) VALUES
+('Baldomero', '12345678');
 
 --
 -- Índices para tablas volcadas
@@ -111,8 +128,19 @@ ALTER TABLE `liga`
 -- Indices de la tabla `resultados`
 --
 ALTER TABLE `resultados`
+  ADD PRIMARY KEY (`id_result`),
   ADD KEY `fk_equipo_1` (`cod_equipo1`),
   ADD KEY `fk_equipo_2` (`cod_equipo2`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `resultados`
+--
+ALTER TABLE `resultados`
+  MODIFY `id_result` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Restricciones para tablas volcadas
